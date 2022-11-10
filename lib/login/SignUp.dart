b39769aacebe674cf9046d2a6d28af6e06ee7267
Sangi <sangi.ram16318@gmail.com>
2022-11-10 08:36:15 +0530

    DB link

--- lib/login/SignUp.dart
+++ lib/login/SignUp.dart
@@ -5,9 +5,118 @@ import 'package:fuelit_app/login//LoginScreen.dart';
 import 'package:http/http.dart' as http;
 
 TextEditingController namectl=TextEditingController();
+
+
 class SignUp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+         theme: ThemeData(
+            primarySwatch:Colors.red, //primary color for theme
+         ),
+         home: WriteSQLdata() //set the class here
+    );
+  }
+}
+
+////////////////////////////////////////////////
+///////////////////////////////////////////////
+
+class WriteSQLdata extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+     return WriteSQLdataState();
+  }
+}
+
+//  @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child:
+//             Container(
+//               height: 200,
+//               width: 100,
+//               color:  Colors.yellow,
+//         ),
+//       ),
+//     );
+//   }
+
+
+class WriteSQLdataState extends State<WriteSQLdata>{
+
+  TextEditingController namectl = TextEditingController();
+  // TextEditingController addressctl = TextEditingController();
+  // Textl̥EditingController classctl = TextEditingController();
+  // TextEditingController rollnoctl = TextEditingController();
+  //text controller for TextField
+
+  late bool error, sending, success;
+  late String msg;
+
+  String phpurl = "http://192.168.0.101/test/write.php";
+  // do not use http://localhost/ for your local
+  // machine, Android emulation do not recognize localhost
+  // insted use your local ip address or your live URL
+  // hit "ipconfig" on Windows or  "ip a" on Linux to get IP Address
 
   @override
+  void initState() {
+      error = false;
+      sending = false;
+      success = false;
+      msg = "";
+      super.initState();
+  }
+
+  Future<void> sendData() async {
+
+     var res = await http.post(Uri.parse(phpurl), body: { 
+          "name": namectl.text,
+          // "address": addressctl.text,
+          // "class": classctl.text,
+          // "rollno": rollnoctl.text,
+      }); //sending post request with header data
+
+     if (res.statusCode == 200) {
+       print(res.body); //print raw response on console
+       var data = json.decode(res.body); //decoding json to array
+       if(data["error"]){
+          setState(() { //refresh the UI when error is recieved from server
+             sending = false;
+             error = true;
+             msg = data["message"]; //error message from server
+          });
+       }else{
+         
+         namectl.text = "";
+        //  addressctl.text = "";
+        //  classctl.text = "";
+        //  rollnoctl.text = "";
+         //after write success, make fields empty
+
+          setState(() {
+             sending = false;
+             success = true; //mark success and refresh UI with setState
+          });
+       }
+       
+    }else{
+       //there is error
+        setState(() {
+            error = true;
+            msg = "Error during sendign data.";
+            sending = false;
+            //mark error and refresh UI with setState
+        });
+    }
+  }
+
+
+
+
+ @override
   Widget build(BuildContext context) {
     double width=MediaQuery.of(context).size.width;
     double height=MediaQuery.of(context).size.height;
@@ -132,8 +241,12 @@ class SignUp extends StatelessWidget{
                       ElevatedButton(
                         child: Text('SignUp'),
                         style: ElevatedButton.styleFrom(backgroundColor: Color(0xffEE7B23)),
-                        onPressed: (){
-                          UserAdd();
+                        onPressed: (){ //if button is pressed, setstate sending = true, so that we can show "sending..."
+                          setState(() {
+                             sending = true;
+                          });
+                          sendData();
+                        
                           Navigator.push(
                             context,
                             MaterialPageRoute(builder: (context) => LoginScreen()),
@@ -173,6 +286,12 @@ class SignUp extends StatelessWidget{
   }
 }
 
+// Future getData() async{
+  //   var url = 'https://192.168.93.39/write_new.php';
+  //   http.Response response = await http.get(url);
+  //   var data = jsonDecode(response.body);
+  //   print(data.toString());
+  // }
 
 // class Second extends StatefulWidget {
 //   @override
@@ -278,30 +397,27 @@ class SignUp extends StatelessWidget{
 //   }
 // }
 
-class UserAdd extends StatelessWidget
-{
-
-  // bool valname=false;
-  String phpurl="http://192.168.122.213/write_new.php";
-  Future sendData() async
-  {
-    String name = namectl.text;
-    var Data={ 'name' : name};
-    var res=await http.post(Uri.parse(phpurl),body: {
-      "name": namectl.text.toString(),
-    };
-    var message = jsonDecode(res.body);
-
-    if (message == "true") {
-      print("Successful " + message);
-    } else {
-      print("Error: " + message);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold();
-  }
-}
+// class UserAdd extends StatelessWidget
+// {
+// bool valname=false;
+//   String phpurl="http://192.168.93.39/write_new.php";
+//   Future sendData() async
+//   {
+//     String name = namectl.text;
+//     var Data={ 'name' : name};
+//     var res=await http.post(Uri.parse(phpurl),body: {
+//       "name": namectl.text.toString(),
+//     };
+//     var message = jsonDecode(res.body);
+//     if (message == "true") {
+//       print("Successful " + message);
+//     } else {
+//       print("Error: " + message);
+//     }
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return Scaffold();
+//   }
+// }
