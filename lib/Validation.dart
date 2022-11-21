14f1ec82e181f2207b8ba1bd8773b03efec164a2
Sangi <sangi.ram16318@gmail.com>
2022-11-22 01:54:06 +0530

    Validation

---
+++ lib/Validation.dart
@@ -0,0 +1,29 @@
+public class Validation
+{
+  bool isValidEmail(String mailid) {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(mailid);
+  }
+
+
+
+  bool  isValidName(name){
+    final nameRegExp = new RegExp(r"^\s*([A-Za-z]{1,}([\.,] |[-']| ))+[A-Za-z]+\.?\s*$");
+    return nameRegExp.hasMatch(name);
+  }
+  
+  bool  isValidPassword(pass){
+    final passwordRegExp =
+    RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\><*~]).{8,}/pre>');
+    return passwordRegExp.hasMatch(pass);
+  }
+  
+  bool  isNotNull(nullval){
+    return nullval!=null;
+  }
+  
+  bool  isValidPhone(phone){
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(phone);
+    }
+}
