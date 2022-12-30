7985baee3c38f0b16d2302803e77b097de66f885
Sangi <sangi.ram16318@gmail.com>
2022-12-30 22:52:11 +0530

    Map

--- lib/userhome/NearestPetrolPump.dart
+++ lib/userhome/NearestPetrolPump.dart
@@ -1,18 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+ 
+// function to trigger the build process
+void main() => runApp(const NearestPetrolPump());
+ 
+_launchURLBrowser() async {
+  var url = 'https://www.google.com/maps/search/nearest+petrol+pumps';
+  if (await canLaunch(url)) {
+    await launch(url);
+    } 
+    else {
+      throw 'Could not launch $url';
+      }
+}
 
+
+_launchURLApp() async {
+  var url ="https://www.google.com/maps/search/nearest+petrol+pumps/";
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+ 
 class NearestPetrolPump extends StatelessWidget {
-
+  const NearestPetrolPump({Key? key}) : super(key: key);
+ 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xffC4DFCB),
-      child: Center(
-        child: Text(
-          "Page Number 1",
-          style: TextStyle(
-            color: Colors.green[900],
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Geeks for Geeks'),
+          backgroundColor: Colors.green,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Container(
+                  height: 250.0,
+                ),
+                Container(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: _launchURLBrowser,
+                  style: ButtonStyle(
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(5.0)),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  // textColor: Colors.black,
+                  // padding: const EdgeInsets.all(5.0),
+                  child: const Text('Open in Browser'),
+                ),
+                Container(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  onPressed: _launchURLApp,
+                  style: ButtonStyle(
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(5)),
+                      textStyle: MaterialStateProperty.all(
+                          const TextStyle(color: Colors.black))),
+                  // textColor: Colors.black,
+                  // padding: const EdgeInsets.all(5.0),
+                  child: const Text('Open in App'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
