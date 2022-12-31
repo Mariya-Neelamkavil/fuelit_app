e2a7fb868d98c89bc9a30e4a7c715c5c293c8e19
Sangi <sangi.ram16318@gmail.com>
2022-12-31 11:42:59 +0530

    Map

--- lib/userhome/NearestPetrolPump.dart
+++ lib/userhome/NearestPetrolPump.dart
@@ -1,82 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
- 
-// function to trigger the build process
-void main() => runApp(const NearestPetrolPump());
- 
-_launchURLBrowser() async {
-  var url = 'https://www.google.com/maps/search/nearest+petrol+pumps';
-  if (await canLaunch(url)) {
-    await launch(url);
-    } 
-    else {
-      throw 'Could not launch $url';
-      }
-}
 
-
-_launchURLApp() async {
-  var url ="https://www.google.com/maps/search/nearest+petrol+pumps/";
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
- 
 class NearestPetrolPump extends StatelessWidget {
-  const NearestPetrolPump({Key? key}) : super(key: key);
- 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Geeks for Geeks'),
-          backgroundColor: Colors.green,
+    return  MaterialApp(
+        theme: ThemeData(
+          primarySwatch:Colors.blueGrey, //primary color for theme
         ),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  height: 250.0,
-                ),
-                Container(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: _launchURLBrowser,
-                  style: ButtonStyle(
-                    padding:
-                        MaterialStateProperty.all(const EdgeInsets.all(5.0)),
-                    textStyle: MaterialStateProperty.all(
-                      const TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  // textColor: Colors.black,
-                  // padding: const EdgeInsets.all(5.0),
-                  child: const Text('Open in Browser'),
-                ),
-                Container(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed: _launchURLApp,
-                  style: ButtonStyle(
-                      padding:
-                          MaterialStateProperty.all(const EdgeInsets.all(5)),
-                      textStyle: MaterialStateProperty.all(
-                          const TextStyle(color: Colors.black))),
-                  // textColor: Colors.black,
-                  // padding: const EdgeInsets.all(5.0),
-                  child: const Text('Open in App'),
-                ),
-              ],
-            ),
-          ),
+     home: ElevatedButton(
+          onPressed: _launchUrl,
+          child: Text('Go to Google Map'),
         ),
-      ),
-    );
+      );
   }
+}
+
+_launchUrl() async {
+  const url = 'https://www.google.com/maps/search/nearest+petrol+pumps/';
+  await launch(url);
 }
